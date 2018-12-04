d8e267346f804eb5d982688e84ec177ecaed4a8a
George Berezhnoy <gohabereg@users.noreply.github.com>
2018-12-04 20:07:10 +0300

    Update Toolbox API (#13)
    * Update Toolbox API
    
    * Bump version

--- src/index.js
+++ src/index.js
@@ -26,15 +26,6 @@ require('./index.css').toString();
  */
 class Header {
   /**
-   * Should this tools be displayed at the Editor's Toolbox
-   * @returns {boolean}
-   * @public
-   */
-  static get displayInToolbox() {
-    return true;
-  }
-
-  /**
    * Render plugin`s main Element and fill it with saved data
    *
    * @param {{data: HeaderData, config: HeaderConfig, api: object}}
@@ -389,11 +380,17 @@ class Header {
   }
 
   /**
-   * Get Tool icon's SVG
-   * @return {string}
+   * Get Tool toolbox settings
+   * icon - Tool icon's SVG
+   * title - title to show in toolbox
+   *
+   * @return {{icon: string, title: string}}
    */
-  static get toolboxIcon() {
-    return '<svg width="11" height="14" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M7.6 8.15H2.25v4.525a1.125 1.125 0 0 1-2.25 0V1.125a1.125 1.125 0 1 1 2.25 0V5.9H7.6V1.125a1.125 1.125 0 0 1 2.25 0v11.55a1.125 1.125 0 0 1-2.25 0V8.15z"/></svg>';
+  static get toolbox() {
+    return {
+      icon: '<svg width="11" height="14" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M7.6 8.15H2.25v4.525a1.125 1.125 0 0 1-2.25 0V1.125a1.125 1.125 0 1 1 2.25 0V5.9H7.6V1.125a1.125 1.125 0 0 1 2.25 0v11.55a1.125 1.125 0 0 1-2.25 0V8.15z"/></svg>',
+      title: 'Header'
+    };
   }
 }
 
