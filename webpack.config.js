a2636552c5f5184ecf13fef73e1b123ee31b790a
SoSIE <sosie@sos-productions.com>
2020-10-27 12:09:19 +0100

    Package version in Bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,90 @@
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            query: {
-              presets: [ '@babel/preset-env' ],
+/**
+ * Webpack configuration
+ *
+ * @author Codex Team
+ * @copyright Khaydarov Murod
+ */
+'use strict';
+
+module.exports = (env, argv) => {
+  const path = require('path');
+  const pkg = require('./package.json');
+
+  /**
+   * Environment
+   *
+   * @type {any}
+   */
+  const NODE_ENV = argv.mode || 'development';
+  const VERSION = process.env.VERSION || pkg.version;
+
+  /**
+   * Plugins for bundle
+   *
+   * @type {webpack}
+   */
+  const webpack = require('webpack');
+
+
+    return  {
+    entry: [ './src/index.js' ],
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+            {
+                loader: 'babel-loader',
+                query: {
+                presets: [ '@babel/preset-env' ],
+                },
             },
-          },
+            ]
+        },
+        {
+            test: /\.css$/,
+            use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                plugins: [
+                    require('postcss-nested-ancestors'),
+                    require('postcss-nested')
+                ]
+                }
+            }
+            ]
+        },
+        {
+          test: /\.(svg)$/,
+          use: [
+            {
+              loader: 'raw-loader',
+            }
+          ]
+        }
         ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(svg)$/,
-        use: [
-          {
-            loader: 'raw-loader',
-          }
-        ]
-      }
-    ]
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js',
-    library: 'Header',
-    libraryTarget: 'umd'
-  }
+    },
+     plugins: [
+      /** Pass variables into modules */
+      new webpack.DefinePlugin({
+        NODE_ENV: JSON.stringify(NODE_ENV),
+        VERSION: JSON.stringify(VERSION),
+      }),
+
+      new webpack.BannerPlugin({
+        banner: `Header tool\n\n@version ${VERSION}\n\n@package https://github.com/editor-js/header\n@licence MIT\n@author CodeX <https://codex.so>`,
+      }),
+     ],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js',
+        library: 'Header',
+        libraryTarget: 'umd'
+    }
+  };
 };
