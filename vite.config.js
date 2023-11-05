2fb1c47b830730e1ceb3c67621b7f2c2174f4c39
robonetphy <1998patelumang@gmail.com>
2023-11-05 20:20:33 +0530

    update the package

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default {
     copyPublicDir: false,
     lib: {
       entry: path.resolve(__dirname, "src", "index.js"),
-      name: "Paragraph",
-      fileName: "paragraph",
+      name: "Header",
+      fileName: "header",
     },
   },
   define: {
