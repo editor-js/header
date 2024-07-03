f68441ec27928f2a3d78ef53858f288ed6430cb9
dependentmadani <madani.badaoui12@gmail.com>
2024-07-03 22:57:20 +0100

    fix(undefined data): undefined issue in normalizeData member function

--- src/index.ts
+++ src/index.ts
@@ -145,14 +145,14 @@ export default class Header {
    * @private
    */
   normalizeData(data: HeaderData): HeaderData {
-    const newData: HeaderData = {text: '', level: this.defaultLevel.number };
+    const newData: HeaderData = { text: '', level: this.defaultLevel.number };
 
     if (typeof data !== 'object') {
-      data = {text: '', level: this.defaultLevel.number};
+      data = { text: '', level: this.defaultLevel.number};
     }
 
     newData.text = data.text || '';
-    newData.level = parseInt(data.level.toString()) || this.defaultLevel.number;
+    newData.level = data.level ? parseInt(data.level.toString()) : this.defaultLevel.number;
 
     return newData;
   }
